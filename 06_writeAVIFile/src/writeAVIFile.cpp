fd5fd4d4cd35fb4eca1f3fe7c18d976d30536d1c
giulianoc@catrasoftware.it <giulianoc@catrasoftware.it>
2018-12-26 17:53:59 +0100

    Added 06_writeAVIFile

---
+++ 06_writeAVIFile/src/writeAVIFile.cpp
@@ -0,0 +1,77 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+	if (argc != 3)
+	{
+		cerr << "Usage: " << argv[0] << " <in video path name> <out video path name>" << endl;
+		cerr << "\t[ESC]: exit" << endl;
+
+		return 1;
+	}
+
+	string windowName ("WriteFile");
+	string windowNameLogPolar ("WriteFile Log_Polar");
+
+	cv::VideoCapture capture(argv[1]);
+
+	double fps = capture.get(cv::CAP_PROP_FPS);
+	cv::Size size(
+			(int) capture.get(cv::CAP_PROP_FRAME_WIDTH),
+			(int) capture.get(cv::CAP_PROP_FRAME_HEIGHT)
+	);
+
+	cv::VideoWriter writer;
+
+	writer.open(argv[2], cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, size);
+
+	cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+	cv::namedWindow(windowNameLogPolar, cv::WINDOW_AUTOSIZE);
+
+	cv::Mat logPolarFrame;
+	cv::Mat bgrFrame;
+
+	while(true)
+	{
+		capture >> bgrFrame;
+		if (bgrFrame.empty())
+			break;
+
+		cv::imshow(windowName, bgrFrame);
+
+		cv::logPolar(
+				bgrFrame,
+				logPolarFrame,
+				cv::Point2f(			// Center point for log polar transformation 
+					bgrFrame.cols/2,
+					bgrFrame.rows/2
+					),
+				40,						// magnitude (scale parameter)
+				cv::WARP_FILL_OUTLIERS	// fill outliers with 'zero'
+				);
+
+		cv::imshow(windowNameLogPolar, logPolarFrame);
+
+		writer << logPolarFrame;
+
+		char c = (char) cv::waitKey(33);
+		if (c == 27)
+			break;
+	}
+
+	cv::destroyWindow(windowName);
+	cv::destroyWindow(windowNameLogPolar);
+
+	capture.release();
+
+
+	return 0;
+}
+
